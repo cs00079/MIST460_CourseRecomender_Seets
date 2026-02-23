cdca1dd455466d7ba4def628f13ee396926bff0b
Colin <cs00079@mix.wvu.edu>
2026-02-23 18:17:50 -0500

    First Data Programming Objects Push

--- Data/DatabaseProgrammingObjects_Seets.sql
+++ Data/DatabaseProgrammingObjects_Seets.sql
@@ -1,1 +1,19 @@
---Push and commit.
+use MIST460_RDB_Seets
+
+GO
+
+select * 
+from AppUser
+where AppUserID = 1;
+
+select *
+from Student
+where StudentID = 1;
+
+select *
+from Registration
+where StudentID = 1;
+
+select *
+from RegistrationSection
+where RegistrationID = 1;
