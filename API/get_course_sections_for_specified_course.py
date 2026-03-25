d4af1313b9ea4dc2a24398775edfa42ab06f5222
Colin <cs00079@mix.wvu.edu>
2026-03-25 14:45:58 -0400

    API Update

--- API/get_course_sections_for_specified_course.py
+++ API/get_course_sections_for_specified_course.py
@@ -1,28 +1,21 @@
 from get_db_connection import get_db_connection
 
-def get_course_sections_for_specified_course(
-    subject_code: str = None,
-    course_number: str = None,
-):
+
+def get_course_sections_for_specified_course(subject_code, course_number):
+
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("{CALL procGetCourseSectionsForSpecifiedCourse(?, ?)}", (subject_code, course_number))
+
+    query = """
+        SELECT *
+        FROM Section
+        WHERE SubjectCode = ? AND CourseNumber = ?
+    """
+
+    cursor.execute(query, subject_code, course_number)
+
     rows = cursor.fetchall()
+
     conn.close()
 
-    #Convert rows to list of dictionaries
-
-    results = [
-        {
-            "SubjectCode": row.SubjectCode,
-            "CourseNumber": row.CourseNumber,
-            "SectionNumber": row.SectionNumber,
-            "SectionSemester": row.SectionSemester,
-            "SectionYear": row.SectionYear,
-            "RemainingOpenings": row.RemainingOpenings,
-            "InstructorName": row.InstructorName
-        }
-        for row in rows
-    ]
-
-    return {"data": results}
+    return [tuple(row) for row in rows]
