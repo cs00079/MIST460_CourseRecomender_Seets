c61d1e982fe7aad095c2ee748f45f5650854d8c7
Colin <cs00079@mix.wvu.edu>
2026-03-25 15:00:02 -0400

    Swagger Update

--- API/get_course_sections_for_specified_course.py
+++ API/get_course_sections_for_specified_course.py
@@ -1,9 +1,21 @@
+from get_db_connection import get_db_connection
+
+
 def get_course_sections_for_specified_course(subject_code, course_number):
 
-    return [
-        {
-            "subject_code": subject_code,
-            "course_number": course_number,
-            "message": "API WORKS"
-        }
-    ]
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    query = """
+        SELECT *
+        FROM Section
+        WHERE SubjectCode = ? AND CourseNumber = ?
+    """
+
+    cursor.execute(query, subject_code, course_number)
+
+    rows = cursor.fetchall()
+
+    conn.close()
+
+    return [tuple(row) for row in rows]
