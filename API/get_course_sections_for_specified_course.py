549a84cfa1916dbbbd1caf5f5a2070fccd52eca1
Colin <cs00079@mix.wvu.edu>
2026-03-25 14:56:49 -0400

    Hopefully fast API works

--- API/get_course_sections_for_specified_course.py
+++ API/get_course_sections_for_specified_course.py
@@ -1,21 +1,9 @@
-from get_db_connection import get_db_connection
-
-
 def get_course_sections_for_specified_course(subject_code, course_number):
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    query = """
-        SELECT *
-        FROM Section
-        WHERE SubjectCode = ? AND CourseNumber = ?
-    """
-
-    cursor.execute(query, subject_code, course_number)
-
-    rows = cursor.fetchall()
-
-    conn.close()
-
-    return [tuple(row) for row in rows]
+    return [
+        {
+            "subject_code": subject_code,
+            "course_number": course_number,
+            "message": "API WORKS"
+        }
+    ]
