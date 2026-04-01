4e143e086afd7fee3293ab5c239b1e907a53764b
Colin <cs00079@mix.wvu.edu>
2026-04-01 02:45:24 -0400

    Maybe

--- API/validate_user.py
+++ API/validate_user.py
@@ -6,7 +6,7 @@ def validate_user(username: str, password: str,):
   
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("{CALL procValidateUser(?, ?)}", (username, password))
+    cursor.execute("{CALL dbo.procValidateUser(?, ?)}", (username, password))
     rows = cursor.fetchall()
     conn.close()
 
