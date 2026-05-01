1ea0d829a0bed41be4940ffb5a4e869411e21a40
Colin <cs00079@mix.wvu.edu>
2026-05-01 01:24:02 -0400

    Final Push

--- API/course_recommender_api.py
+++ API/course_recommender_api.py
@@ -27,4 +27,8 @@ def validate_user_endpoint(username: str, password: str):
 
 @app.get("/get_course_recommendations_for_selected_job/")
 def get_course_recommendations_for_selected_job_endpoint(job_description: str):
-    return get_course_recommendations_for_selected_job(job_description)
+    return get_course_recommendations_for_selected_job(job_description)@app.get("/get_all_jobs/")
+
+@app.get("/get_all_jobs/")
+def get_all_jobs_endpoint():
+    return get_all_jobs()
